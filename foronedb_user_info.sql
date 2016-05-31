340d9342b378581de3dbe05d28cbdf54e4c93424
gan <283925583@qq.com>
2016-05-31 16:07:46 +0800

    Add files via upload

---
+++ foronedb_user_info.sql
@@ -0,0 +1,63 @@
+-- MySQL dump 10.13  Distrib 5.7.9, for osx10.9 (x86_64)
+--
+-- Host: 127.0.0.1    Database: foronedb
+-- ------------------------------------------------------
+-- Server version	5.7.12
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `user_info`
+--
+
+DROP TABLE IF EXISTS `user_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_info` (
+  `userid` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `username` varchar(45) NOT NULL COMMENT '用户登录名',
+  `password` varchar(45) NOT NULL COMMENT '用户密码',
+  `sex` varchar(45) DEFAULT NULL COMMENT '用户性别',
+  `location` varchar(45) DEFAULT NULL COMMENT '用户地址',
+  `age` varchar(45) DEFAULT NULL COMMENT '年龄',
+  `constellation` varchar(45) DEFAULT NULL COMMENT '星座',
+  `height` varchar(45) DEFAULT NULL COMMENT '身高',
+  `weight` varchar(45) DEFAULT NULL COMMENT '体重',
+  `bloodtype` varchar(45) DEFAULT NULL COMMENT '血型',
+  `personality` varchar(45) DEFAULT NULL COMMENT '个性',
+  `telephone` varchar(45) DEFAULT NULL COMMENT '电话',
+  PRIMARY KEY (`userid`),
+  UNIQUE KEY `userid_UNIQUE` (`userid`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_info`
+--
+
+LOCK TABLES `user_info` WRITE;
+/*!40000 ALTER TABLE `user_info` DISABLE KEYS */;
+INSERT INTO `user_info` VALUES (1,'test1','123','0',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(2,'test2','456','1',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(3,'sss','ss','0',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(8,'ganyi','000','1',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(12,'liuchen','000','1',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(16,'liuchen1','4a7d1ed414474e4033ac29ccb8653d9b',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(19,'liuchen2','4a7d1ed414474e4033ac29ccb8653d9b',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(20,'liuchen3','81dc9bdb52d04dc20036dbd8313ed055',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `user_info` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-05-31 15:42:24
